45726258aca9e84bb86ac4053f3dc6b0c64ee58d
Miguel Angel Escalante Serrato <escala@gmail.com>
2024-11-21 12:33:46 -0600

    add parameters

---
+++ app/parameters.py
@@ -0,0 +1,11 @@
+import os
+
+PROJECT_ID = os.getenv("PROJECT_ID", "metodo-cognitivo")
+API_ENDPOINT = os.getenv("API_ENDPOINT", "")
+LOCATION = os.getenv("LOCATION", "")
+INDEX_ID = os.getenv("INDEX_ID", "")
+INDEX = os.getenv("INDEX", "")
+DEPLOYED_INDEX_ID = os.getenv("DEPLOYED_INDEX_ID", "")
+INDEX_ENDPOINT = os.getenv("INDEX_ENDPOINT", "")
+RAG_CORPORA = os.getenv("RAG_CORPORA", "")
+MODEL_ID = os.getenv("MODEL_ID", "")
