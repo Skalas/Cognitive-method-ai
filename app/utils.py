9b47e4548c00d8a344a8f0e694839156f5396adb
Miguel Angel Escalante Serrato <escala@gmail.com>
2024-11-21 12:34:26 -0600

    remove unused

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,3 @@
-from google.auth import default
-from googleapiclient.discovery import build
-from google.cloud import storage
-from googleapiclient.http import MediaIoBaseDownload
-import os
-import re
-import numpy as np
-
-
 def load_prompt_template(file_path) -> str:
     with open(file_path, "r", encoding="utf-8") as file:
         prompt_template = file.read()
