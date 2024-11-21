69860a575f6aaf22abc0b34cf7f82ee2e49676c9
Miguel Angel Escalante Serrato <escala@gmail.com>
2024-11-20 18:36:07 -0600

    new tests

--- app/utils/test_endpoints.py
+++ app/utils/test_endpoints.py
@@ -2,10 +2,16 @@ import requests
 
 print(requests.get("http://localhost:8000/v1/").json())
 
-
-print(
-    requests.post(
-        "http://localhost:8000/v1/generate-training",
-        json={"question": "What is the capital of France?"},
-    ).json()
-)
+questions = [
+    "Tengo un equipo de 12 jugadores, tienen entre 5 y 6 años, que tipos de ejercicios de técnica me puedes recomendar?",
+    "Tengo un equipo cuyas edades están entre 5 y 6 años, que tipos de ejercicios de técnica me puedes recomendar?",
+    "cuál es la capital de francia?",
+]
+for question in questions:
+    print(question)
+    print(
+        requests.post(
+            "http://localhost:8000/v1/generate-training",
+            json={"question": question},
+        ).json()
+    )
